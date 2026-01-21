45890860576c0d9ad9502a7910c2c9e4ed641ebe
Juscelino <juscelinotrevenzoli@gmail.com>
2026-01-21 17:19:02 -0300

    frist commit!!

---
+++ API/-- SQLite.sql
@@ -0,0 +1,10 @@
+-- SQLite
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ('bob-id', 'Bob', 'bob@test.com');
+
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ('davi-id', 'Davi', 'davi@test.com');
+
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ('flavia-id', 'Flavia', 'flavia@test.com');
+
